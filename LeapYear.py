f835a2774c475871a1bcc05e8d1ac9376f27e639
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2019-12-26 23:32:01 +0530

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,17 +1,19 @@
 #Python Program to Check Leap Year
 class LeapYear:
-    run= True
-    while run:
-        print('\n\t\t@@@Welcome In Number World@@@')
-        try:
-            year = int(input('Enter the year to check given year is leap year or not:= '))
-            if year%4==0 or year%400==0:
-                print('Given year is leap year')
-            elif year%100!=0:
-                print('Given year is not a leap year')
-            else:
-                print('Given year is not a leap year')
-        except:
-            print('Please type only integer value')
+    def checkLeapYear(self):
+        run= True
+        while run:
+            print('\n\t\t@@@Welcome In Year World@@@')
+            try:
+                year = int(input('Enter the year to check if given year is leap year or not:= '))
+                if year%4==0 or year%400==0:
+                    print('This year is leap year')
+                elif year%100!=0:
+                    print('This year is not a leap year')
+                else:
+                    print('This year is not a leap year')
+            except:
+                print('Please type only integer value')
 if __name__ == "__main__":
-    object = LeapYear()
+    LeapYearObject = LeapYear()
+    LeapYearObject.checkLeapYear()
