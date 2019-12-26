8fd754866740b943a72b8697342f63576a3da6b8
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2019-12-26 23:38:15 +0530

    Update Odd&Even.py

--- Odd&Even.py
+++ Odd&Even.py
@@ -1,16 +1,18 @@
 #Python Program to Check if a Number is Odd or Even
 class OddEven:
-    run = True
-    while run:
-        try:
-            num = int(input('Enter num to check given num is odd or even : '))
-            if num%2==0:
-                print('Given Number is Even number')
-            else:
-                print('Given Number is Odd number')
-                run = False
-                print('\t\t****Thanks for running this program***')
-        except:
-            print('\t\t **Enter only integer Number**')
+    def checkOddEven(self):
+        run = True
+        while run:
+            try:
+                num = eval(input('Enter num to check given num is odd or even : '))
+                if num%2==0:
+                    print('Given Number is Even number')
+                else:
+                    print('Given Number is Odd number')
+                    run = False
+                    print('\t\t****Thanks for running this program***')
+            except:
+                print('\t\t @@@Attention : Enter only Number@@@')
 if __name__ == "__main__":
-    obj = OddEven()
+    OddEvenobj = OddEven()
+    OddEvenobj.checkOddEven()
