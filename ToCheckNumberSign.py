04fc02e4c834df8750900f287ce36fe0f8454ea2
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2019-12-26 23:49:58 +0530

    Update NumberType.py

--- ToCheckNumberSign.py
+++ ToCheckNumberSign.py
@@ -1,23 +1,26 @@
 #Python Program to Check if a Number is Positive, Negative or Zero
-class Number:
-    run= True
-    while run:
-        print('\n\t\t@@@Welcome In Number World@@@')
-        try:
-            num = int(input('Enter the number to check it is positive or negative:= '))
-            if num!=0 and num>0:
-                print('#Given number is positive#')
-                print('\t\t\tThank you! Visit Again!')
-                run = False
-            elif num!=0 and num<0:
-                print('#Given number is negative#')
-                print('\t\t\tThank you! Visit Again!')
-                run = False
-            else:
-                print('Given number will be equal to zero')
-                run = False
-        except:
-            print('\t\t Attention: Enter only integer number')
+class NumberType:
+    def checkTypeOfNumber(self):
+        run= True
+        while run:
+            print('\n\t\t@@@Welcome In Number World@@@')
+            try:
+                num = eval(input('Enter the number to check if it is positive or negative:= '))
+                if num>0:
+                    print('#Given number is positive#')
+                    print('\t\t\tThank you! Visit Again!')
+                    run = False
+                elif num<0:
+                    print('#Given number is negative#')
+                    print('\t\t\tThank you! Visit Again!')
+                    run = False
+                else:
+                    print('#Given number is equal to zero#')
+                    run = False
+            except:
+                print('\t\t Attention: Enter only integer number')
 
 if __name__ == "__main__":
-   object1 = Number()
+   NumberTypeObj = NumberType()
+   NumberTypeObj.checkTypeOfNumber()
+
