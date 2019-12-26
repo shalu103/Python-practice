1db366e4a3d28f859be29272c4ce4c19db7a5d7f
Shalu Sharma <59021479+shalu103@users.noreply.github.com>
2019-12-26 23:06:51 +0530

    Update calendar file

--- Calendar.py
+++ Calendar.py
@@ -1,16 +1,18 @@
 # Python program to display calendar
 class calendar:
-    run = True
-    print('\t\t****Welcome In Magic world****')
-    while run:
-        try:
-            import calendar
-            year = int(input('enter the year: '))
-            month = int(input('enter the month'))
-            print(calendar.month(year,month))
-            run = False
-            print('GoodBye!!')
-        except:
-            print('Enter only Integer')
+    def showCalendar(self): 
+        run = True
+        print('\t\t****Welcome In Magic world****')
+        while run:
+            try:
+                import calendar
+                year = int(input('enter the year: '))
+                month = int(input('enter the month'))
+                print(calendar.month(year,month))
+                run = False
+                print('GoodBye!!')
+            except:
+                print('Enter only Integer')
 if __name__ == "__main__":
-  obj = calendar()
+  calendarobj = calendar()
+calendarobj.showCalendar()
